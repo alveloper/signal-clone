df3a0162c27ba0094543219f444d23f9da809628
Suyeon <alveloper@gmail.com>
2021-08-22 16:17:47 +0900

    1. Building the App

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMSDCVdZB2I5ZIx1oHR6hUoC2s1lMucJ4",
+  authDomain: "signal-clone-4c9eb.firebaseapp.com",
+  projectId: "signal-clone-4c9eb",
+  storageBucket: "signal-clone-4c9eb.appspot.com",
+  messagingSenderId: "641544522092",
+  appId: "1:641544522092:web:f7675c96859bc324ff4b49"
+};
